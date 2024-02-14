4506bca58105d140797051a596f0b794769aeb86
Krushna Kulkarni <krushnakulkarni17@gmail.com>
2024-02-15 03:02:10 +0530

    Feat: Implemented Chat functionality

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,34 @@
 const express = require("express");
+const http = require("http");
+const path = require("path");
 
 const app = express();
+const server = http.createServer(app);
+
+// Set Static Folder
+app.use(express.static(path.join(__dirname, "frontend")));
+
+// for connection with client
+const io = require("socket.io")(server, {
+  pingTimeout: 60000,
+  cors: {
+    origin: "http://localhost:3000",
+  },
+});
+
+// Run when client connects
+io.on("connection", (socket) => {
+  // Sending to client - frontend that can be catched by socket.on('variable', cb())
+  socket.emit("message", "welcome to chatsApp");
+
+  // Listen for chatMessage
+  socket.on("send_message", (msgData) => {
+    socket.broadcast.emit("receive_message", msgData);
+  });
+});
 
 const PORT = 5000 || process.env.PORT;
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`server running on port ${PORT}`);
 });
--- frontend/src/pages/Chat.js
+++ frontend/src/pages/Chat.js
@@ -1,10 +1,34 @@
-import React from "react";
+import moment from "moment";
+import React, { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
-
+import io from "socket.io-client";
 export const Chat = () => {
   const { userName } = useParams();
+  const [msg, setMsg] = useState("");
+  const [messages, setMessages] = useState([]);
 
   const Navigate = useNavigate();
+
+  const socket = io("http://localhost:5000");
+  useEffect(() => {
+    socket.on("message", (message) => console.log(message));
+    socket.on("receive_message", (newMsg) => {
+      setMessages([...messages, newMsg]);
+    });
+    // eslint-disable-next-line
+  }, [socket]);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    // emitting a message to server
+    socket.emit("send_message", {
+      message: msg,
+      sender: userName,
+      time: moment(Date.now()).format("LT"),
+    });
+    setMsg("");
+  };
 
   return (
     <div className="max-w-[1100px] bg-white my-8 mx-auto overflow-hidden">
@@ -17,31 +41,23 @@ export const Chat = () => {
           Exit Chat
         </button>
       </header>
-      <main className="block md:grid md:grid-cols-[1fr_3fr]">
-        <div className="hidden md:block bg-dark-color-b text-white py-5 pb-16 overflow-y-scroll">
-          <div className="mb-4 px-4">Users</div>
-          <ul className="list-none" id="users">
-            <li className="py-2 px-4">Brad</li>
-            <li className="py-2 px-4">John</li>
-            <li className="py-2 px-4">Mary</li>
-            <li className="py-2 px-4">Paul</li>
-            <li className="py-2 px-4">Mike</li>
-          </ul>
-        </div>
+      <main className="block">
         <div className="p-7 max-h-[500px] overflow-y-scroll">
-          <div className="p-3 mb-4 bg-light-color rounded-md">
-            <p className="text-md font-bold text-dark-color-a mb-2">
-              Brad <span className="text-dark-color-b  text-xs">9:12pm</span>
-            </p>
-            <p className="text">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi,
-              repudiandae.
-            </p>
-          </div>
+          {messages.map((newMessage, index) => (
+            <div key={index} className="p-3 mb-4 bg-light-color rounded-md">
+              <p className="text-md font-bold text-dark-color-a mb-2">
+                {newMessage.sender}{" "}
+                <span className="text-dark-color-b  text-xs">
+                  {newMessage.time}
+                </span>
+              </p>
+              <p className="text">{newMessage.message}</p>
+            </div>
+          ))}
         </div>
       </main>
       <div className="py-5 px-7 bg-dark-color-a">
-        <form className="flex" id="chat-form">
+        <form className="flex" id="chat-form" onSubmit={submitHandler}>
           <input
             className="text-md p-2 mr-2 h-10 flex-1 rounded-md outline-none"
             id="msg"
@@ -49,8 +65,13 @@ export const Chat = () => {
             placeholder="Enter Message"
             required
             autoComplete="off"
+            value={msg}
+            onChange={(e) => setMsg(e.target.value)}
           />
-          <button className="cursor-pointer py-1 px-4 text-dark-color-a bg-light-color text-md border-0 rounded-md">
+          <button
+            type="submit"
+            className="cursor-pointer py-1 px-4 text-dark-color-a bg-light-color text-md border-0 rounded-md"
+          >
             Send
           </button>
         </form>
