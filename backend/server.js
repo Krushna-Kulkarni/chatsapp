f2741705c3110a02bf56d68464ce3f859097385d
Krushna Kulkarni <krushnakulkarni17@gmail.com>
2024-02-15 03:14:16 +0530

    deployment

--- backend/server.js
+++ backend/server.js
@@ -5,9 +5,22 @@ const path = require("path");
 const app = express();
 const server = http.createServer(app);
 
-// Set Static Folder
-app.use(express.static(path.join(__dirname, "frontend")));
+// ----------------------- Deployment ------------------------
 
+const __dirname1 = path.resolve();
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "/frontend/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running...");
+  });
+}
+
+// -----------------------------------------------------
 // for connection with client
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
